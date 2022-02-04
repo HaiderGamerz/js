9372db86f4843df1c6f1dd1ccb2acd5c30d846b5
Haider_Gamerzz <91683909+HaiderGamerz@users.noreply.github.com>
2022-02-04 22:48:34 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+//Q1
+
+// x = 9;
+// y = 7;
+
+// if(x<y){
+//     console.log('x is smaller')
+// }
+// else{
+//     console.log('y is smaller')
+// }
+
+//Q2
+// 2.	 Write a JavaScript program which compute, the average marks of the following students Then, this average is used to determine the corresponding grade
+
+// let students = [
+//     {
+//         id: 1,
+//         studentName: "John",
+//         rollNo: 4,
+//         section: "VD",
+//         totalNumberObtained: 400
+//     },
+//     {
+//         id: 2,
+//         studentName: "Ethan",
+//         rollNo: 2,
+//         section: "VE",
+//         totalNumberObtained: 200
+//     },
+//     {
+//         id: 3,
+//         studentName: "Steve",
+//         rollNo: 10,
+//         section: "VA",
+//         totalNumberObtained: 300
+//     },
+// ]
+// let total = 0;
+// let avg=0
+// students.map((val) => {
+//     total+=val.totalNumberObtained
+//     avg = (total / students.length);
+// })
+
+
+
+// console.log("Average grade: " + (total) / students.length);
+
+//Q3
+
+// let num=5
+// if(num %2 == 0){
+//     console.log('ur number is even')
+// }
+// else{
+//     console.log('ur number is odd')
+// }
+
+//Q4 I CANT DO IT
+//Q5 I CANT DO IT
+//Q6 CANT DO IT
+
+//Q7
+
+// let num=0
+
+// if(num >0){
+//     console.log('number is +')
+// }
+// else if(num <0){
+//     console.log('number is +')
+// }
+// else if(num == 0){
+//     console.log('number is 0')
+// }
+
+//Q8 
+
+let num =10
+if(num % 5 ==0 && num %10 ==0 ){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
